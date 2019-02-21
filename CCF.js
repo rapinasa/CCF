832538755a29f2ab7ead005975c2c2000aa931bf
rapinasa <rapina1990@gmail.com>
2019-02-21 09:32:36 +0200

    modified:   CCF.css 	modified:   CCF.html 	modified:   CCF.js 	modified:   jqueryCCF.js

--- CCF.js
+++ CCF.js
@@ -1,16 +1,20 @@
-
 var myArr = [];
 
 function saveMoney(){
   var totalMoney = document.getElementById("money").value;
-myArr.unshift(parseInt(totalMoney));
-	
-	document.getElementById("total").innerHTML ="Common sum: "+ myArr[0];
+  myArr.unshift(parseInt(totalMoney));
+  document.getElementById("total").innerHTML ="Common sum: "+ myArr[0];
 }
-
 
 function totalFriends(){
 var b = document.getElementById('friends').value;
+
+if ( b>5&&b<1){
+	alert("Number of friends should be no more 5. Sorry...")
+}
+
+console.log(typeof(b));
+
 	switch(b){
 		case '1':
 		document.getElementById("friend1").style.display = "block";
@@ -40,7 +44,7 @@ var b = document.getElementById('friends').value;
 		document.getElementById("thirdSpender").style.display = "block";
 		document.getElementById("fourthSpender").style.display = "block";
 		break;
-			case '5':
+		case '5':
 		document.getElementById("friend1").style.display = "block";
 		document.getElementById("friend2").style.display = "block";
 		document.getElementById("friend3").style.display = "block";
@@ -54,17 +58,10 @@ var b = document.getElementById('friends').value;
 		break;
 		default:
 		document.getElementById("totalInput").style.display = "none";
-
 	}
 }
 
-function changeName(){
-document.getElementById("friendsName").placeholder = "Type name here..";
-
-}
-
 function calculation(){
-    
 	var a = myArr[0];
 	var b = document.getElementById('friends').value;
 	var friend1 = document.getElementById('fr1').value;
@@ -72,8 +69,7 @@ function calculation(){
 	var friend3 = document.getElementById('fr3').value;
 	var friend4 = document.getElementById('fr4').value;
 	var friend5 = document.getElementById('fr5').value;
-		
-	
+     
 myArr.push(parseInt(friend1),parseInt(friend2),parseInt(friend3),parseInt(friend4),parseInt(friend5));
 
  for (i=0; i<myArr.length; i++){
@@ -84,15 +80,14 @@ myArr.push(parseInt(friend1),parseInt(friend2),parseInt(friend3),parseInt(friend
             myArr [j]= bols;} 
     }
    }
-    console.log(myArr);
 
+    console.log(myArr);
 
 if (myArr[0] !== (myArr[1]+myArr[2]+myArr[3]+myArr[4]+myArr[5]))
 {
 return alert("Enter a correct number");
 }
-var avarageSum = (a/b).toFixed(2);
-
+var avarageSum = +(a/b).toFixed(2);
  document.getElementById("firstSpender").innerHTML = " First spender: " + myArr[1]+ " You can get: "+ Math.abs(myArr[1]-avarageSum).toFixed(2);
 
 if ((myArr[2]-avarageSum)>0){
@@ -109,13 +104,25 @@ else{
 	document.getElementById("thirdSpender").innerHTML = " Third spender: " + myArr[3]+ " You can lose: "+ Math.abs(myArr[3]-avarageSum).toFixed(2);
 }
 
+if ((myArr[4]-avarageSum)>0){
+ document.getElementById("fourthSpender").innerHTML = " Fourth spender: " + myArr[4]+ " You can get: "+ Math.abs(myArr[4]-avarageSum).toFixed(2);
+}
+else{
+	document.getElementById("fourthSpender").innerHTML = " Fourth spender: " + myArr[4]+ " You can lose: "+ Math.abs(myArr[4]-avarageSum).toFixed(2);
+}
+
+if ((myArr[5]-avarageSum)>0){
+ document.getElementById("fifthSpender").innerHTML = " Fifth spender: " + myArr[5]+ " You can get: "+ Math.abs(myArr[5]-avarageSum).toFixed(2);
+}
+else{
+	document.getElementById("fifthSpender").innerHTML = " Fifth spender: " + myArr[5]+ " You can lose: "+ Math.abs(myArr[5]-avarageSum).toFixed(2);
+}
 
 document.getElementById("dividetSum").innerHTML = "Avarage sum per person: " + avarageSum;
 	 
 	  
 	 
-	  document.getElementById("fourthSpender").innerHTML = "Fourth spender: " + myArr[4];
-	  document.getElementById("fourthSpender").innerHTML = "Fourth spender: " + myArr[5];
+
 }
 /*
 function saveArr(){
@@ -127,4 +134,3 @@ var JSONReady = localStorage.getItem("myKey");
 myArr = JSON.parse(JSONReady);
 }
 getArr();*/
-
--- jqueryCCF.js
+++ jqueryCCF.js
@@ -9,18 +9,51 @@ $(document).ready(function(){
 
 $(document).ready(function(){
   $("#button-friends").click(function(){
-    $("#button-namefriends").show("high");
+     $("#question2, #button-friends, #friends").hide("high", function(){
+    $("#button-namefriends, #question5, #friendsName").show("high");
   });
 });
-
-
-
+});
 
 $(document).ready(function(){
   $("#button-namefriends").click(function(){
-    $("#button-namefriends,#question2,#question4,#friend1,#friend2,#friend3,#friend4,#friends5").hide("high");
+    $("#button-namefriends,#friendsName,#question2,#question4,#question5,#friend1,#friend2,#friend3,#friend4,#friend5").hide("high");
   });
 });
+
+  var friendsName = document.getElementById('friendsName');
+
+$(document).ready(function(){
+  $("#friendsName1").click(function(){
+    $("#fr1").attr("placeholder", friendsName.value);
+  });
+});
+
+$(document).ready(function(){
+  $("#friendsName2").click(function(){
+    $("#fr2").attr("placeholder", friendsName.value);
+  });
+});
+
+$(document).ready(function(){
+  $("#friendsName3").click(function(){
+    $("#fr3").attr("placeholder", friendsName.value);
+  });
+});
+
+$(document).ready(function(){
+  $("#friendsName4").click(function(){
+    $("#fr4").attr("placeholder", friendsName.value);
+  });
+});
+
+$(document).ready(function(){
+  $("#friendsName5").click(function(){
+    $("#fr5").attr("placeholder", friendsName.value);
+  });
+});
+
+$('input:text').focus(function(){ $(this).val('');});
 
 
 
